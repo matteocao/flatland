a88fe3438162526abf53a71480615dd4a90f3f86
m.caorsi <m.caorsi@giotto.ai>
2025-06-12 14:26:55 +0200

    improve client launch

--- flatland/multiplayer/__main__.py
+++ flatland/multiplayer/__main__.py
@@ -1,4 +1,12 @@
+import argparse
+
 from flatland.multiplayer.client import GameClient
 
 if __name__ == "__main__":
-    GameClient("0.0.0.0", 12345).run()
+    parser = argparse.ArgumentParser(description="Run Flatland Game Client")
+    parser.add_argument("--ip", type=str, default="0.0.0.0", help="Server IP address")
+    parser.add_argument("--port", type=int, default=12345, help="Server port")
+
+    args = parser.parse_args()
+
+    GameClient(args.ip, args.port).run()
